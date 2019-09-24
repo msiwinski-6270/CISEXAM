db6f7f28ce56c57fc0d60bc3e4b05d64b4326486
msiwinski-6270 <55165755+msiwinski-6270@users.noreply.github.com>
2019-09-24 19:48:34 -0400

    Add files via upload

--- Exam_Q22.py
+++ Exam_Q22.py
@@ -4,8 +4,8 @@ cambridge = pd.read_csv('Cambridge_Salaries.csv') #importing cambridge dataset
 
 general = cambridge.describe() #creating a variable with general statistics
 
-grouped = cambridge.groupby("Division")
-totalgroupby = grouped.sum("Total Salary")
+grouped = cambridge.groupby("Division") #group data by division
+totalgroupby = grouped.sum("Total Salary") # calculate totals of the total salary field by the grouped divisions
 
 print("The dataset has the following statistics")
 print(general)
